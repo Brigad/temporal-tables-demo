92269e76e78bb90142d40d4ab7a6d5eb3cb2caab
stefan_l <leon@brigad.co>
2019-06-26 18:52:09 +0200

    feat: bootstrap

---
+++ models/product.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+
+class Product extends Sequelize.Model {
+  static tableName() {
+    return "products";
+  }
+  static attributes() {
+    return {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING
+      }
+    };
+  }
+}
+
+module.exports = (sequelize) => {
+	Product.init(Product.attributes(), { sequelize, modelName: Product.tableName() });
+	return Product;
+}
---
+++ utils/sequelize-loader.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+    
+})
+
+module.exports;
